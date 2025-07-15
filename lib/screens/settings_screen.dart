ce3263866c3322d1e8f94cde9d5ddd08057a85e8
Islam Saeed <eslam20191700106@cis.asu.edu.eg>
2025-07-15 11:50:15 +0300

    Login is disabled now

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -81,7 +81,7 @@ class SettingsScreen extends ConsumerWidget {
 
     ref.read(lastTapTimeProvider.notifier).state = now;
     final AuthChecker _authChecker =  AuthChecker();
-    if (tapCount.state >= -1 && !_authChecker.isUserAuthenticated()) {
+    if (tapCount.state <= -1 && !_authChecker.isUserAuthenticated()) {
       ref.read(isLoadingProvider.notifier).state = false;
       tapCount.state = 0;
       Navigator.of(context).push(
